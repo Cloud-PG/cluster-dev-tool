7f0fc83d8b0b42b430ebe523adce8bbfb7656942
MircoT <mirco.theone@gmail.com>
2018-07-31 17:13:25 +0200

    Update discovery commands

--- discovery/__main__.py
+++ discovery/__main__.py
@@ -22,49 +22,67 @@ def main():
     parser = argparse.ArgumentParser(
         prog='discovery', argument_default=argparse.SUPPRESS)
 
+    ##
+    # discovery inventory
     parser.add_argument('inventory', metavar="filename",
                         default=None, help='Configuration of infrastuctures and entrypoints')
-    parser.set_defaults(cmd=None)
+    
+    ##
+    # discovery inventory sub_command
+    subparsers = parser.add_subparsers(
+        help='Cluster setup commands', dest="sub_command")
 
-    subparsers = parser.add_subparsers(help='Cluster setup commands')
+    ##
+    # discovery inventory sub_command sub_parser_command
 
+    # sub command [commander]
     parser_commander = subparsers.add_parser(
         'commander', help='explore inventory commander')
-    parser_commander.set_defaults(cmd="commander")
-    parser_commander.add_argument(
-        'commander_command', metavar="command", choices=['ls'], default="ls",
-        type=str, help='Command to execute with commander resources. Possible values:["ls"]')
+    sub_parser_commander = parser_commander.add_subparsers(dest="sub_command_commander")
+    parser_commander_show = sub_parser_commander.add_parser('show', help='Get info about commanders')
+    parser_commander_show.add_argument(
+        'parser_commander_show', metavar="target",
+        type=str, help='Target of show command for commanders. Possible values: ["all"]')
 
+    # sub command [infrastructure]
     parser_infrastructure = subparsers.add_parser(
         'infrastructure', help='explore inventory infrastructure')
-    parser_infrastructure.set_defaults(cmd="infrastructure")
-    parser_infrastructure.add_argument(
-        'infrastructure_command', metavar="command", choices=['ls', 'show'], default="ls",
-        type=str, help='Command to execute with infrastructure resources. Possible values:["ls"]')
+    sub_parser_infrastructure = parser_infrastructure.add_subparsers(dest="sub_command_infrastructure")
+    parser_infrastructure_show = sub_parser_infrastructure.add_parser('show', help='Get info about infrastructures')
+    parser_infrastructure_show.add_argument(
+        'parser_infrastructure_show', metavar="target",
+        type=str, help='Target of show command for infrastructures. Possible values: ["all"]')
 
     args, _ = parser.parse_known_args()
+
+    print(args)
 
     with open(args.inventory) as inventory_file:
         inventory = json.load(inventory_file)
 
     # RUN
-    if args.cmd == "commander":
-        if args.commander_command == 'ls':
-            show(
-                colored("[Discovery]", "magenta"),
-                colored("[commander]", "cyan", attrs=['bold']),
-                colored("[ls]", "green"),
-                colored(print_list(inventory['commanders'].keys()), "blue")
-            )
-    elif args.cmd == "infrastructure":
-        if args.infrastructure_command == 'ls':
-            show(
-                colored("[Discovery]", "magenta"),
-                colored("[infrastructure]", "cyan", attrs=['bold']),
-                colored("[ls]", "green"),
-                colored(print_list(
-                        inventory['infrastructures'].keys()), "blue")
-            )
+    if args.sub_command == "commander":
+        if args.sub_command_commander == 'show':
+            if args.parser_commander_show == 'all':
+                show(
+                    colored("[Discovery]", "magenta"),
+                    colored("[commander]", "cyan", attrs=['bold']),
+                    colored("[ls]", "green"),
+                    colored(print_list(inventory['commanders'].keys()), "blue")
+                )
+            else:
+                parser.print_help()
+    elif args.sub_command == "infrastructure":
+        if args.sub_command_infrastructure == 'show':
+            if args.parser_infrastructure_show == 'all':
+                show(
+                    colored("[Discovery]", "magenta"),
+                    colored("[infrastructure]", "cyan", attrs=['bold']),
+                    colored("[ls]", "green"),
+                    colored(print_list(inventory['infrastructures'].keys()), "blue")
+                )
+            else:
+                parser.print_help()
     else:
         parser.print_help()
 
